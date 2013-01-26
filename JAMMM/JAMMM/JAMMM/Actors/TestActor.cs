08cd448429793cf2224932c5c9a40500c2e804a8
MChu <emptym4nd@gmail.com>
2013-01-26 15:36:45 -0800

    add TestActor.cs

---
+++ JAMMM/JAMMM/JAMMM/Actors/TestActor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace JAMMM.Actors
+{
+    class TestActor : Actor
+    {
+        TestActor()
+        {
+            this.Position = new Vector2(1,1);
+            this.
+        }
+    }
+}
